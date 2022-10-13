407d0474a0d01249e258b5e61324d34d9676e1cd
Zakhar Deyneka <95752742+zakhard90@users.noreply.github.com>
2022-10-13 02:16:30 +0200

    Refactored NFT minting to also transfer it to a second wallet

---
+++ src/CustomRedeemer.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude      #-}
+{-# LANGUAGE TemplateHaskell        #-}
+
+module CustomRedeemer where
+
+import qualified PlutusTx
+import           PlutusTx.Prelude
+import           Ledger             (TxOutRef, TokenName, PaymentPubKeyHash)
+
+-- Defining custom redeemer
+data RedeemerParam = RP
+                   { rOutRef :: TxOutRef
+                   , rTokenName :: TokenName
+                   , rPkHash :: PaymentPubKeyHash
+                   }  
+
+PlutusTx.makeIsDataIndexed ''RedeemerParam [('RP,0)]
--- src/NftUpgraderContract.hs
+++ src/NftUpgraderContract.hs
@@ -1,111 +1,144 @@
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE DeriveAnyClass      #-}
-{-# LANGUAGE DeriveGeneric       #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE NoImplicitPrelude   #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE TypeApplications    #-}
-{-# LANGUAGE TypeFamilies        #-}
-{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE DeriveAnyClass         #-}
+{-# LANGUAGE DeriveGeneric          #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE NoImplicitPrelude      #-}
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE TemplateHaskell        #-}
+{-# LANGUAGE TypeApplications       #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE NumericUnderscores     #-}
 
 module NftUpgraderContract where
 
-import Prelude (IO, Semigroup (..), Show (..), String)
-import GHC.Generics (Generic)
-import Data.Aeson (FromJSON, ToJSON)
-import qualified Data.Map as Map
-import Data.Text (Text)
-import Data.Void (Void)
-import Control.Monad hiding (fmap)
-import Text.Printf (printf)
-
-import Plutus.Contract as Contract
-import Plutus.Trace.Emulator as Emulator
+import           Control.Monad                  hiding (fmap)
+import           Data.Aeson                     (ToJSON, FromJSON)
+import           Data.Map                       as Map
+import           Data.Text                      (Text)
+import           Data.Void                      (Void)
+import           Data.Maybe
+import           GHC.Generics                   (Generic)
+import           Plutus.Contract                as Contract
+import           Plutus.Trace.Emulator          as Emulator
 import qualified PlutusTx
-import PlutusTx.Prelude hiding (Semigroup(..), unless)
-import Ledger hiding (mint, singleton)
-import Ledger.Constraints as Constraints
-import qualified Ledger.Typed.Scripts as Scripts
-import Ledger.Value as Value
-
-import Wallet.Emulator.Wallet
+import           PlutusTx.Prelude               hiding (Semigroup(..), unless)
+import qualified PlutusTx.Builtins              as Builtins
+import           Ledger                         hiding (mint, singleton)
+import           Ledger.Constraints             as Constraints
+import qualified Ledger.Typed.Scripts           as Scripts
+import           Ledger.Ada                     as Ada
+import           Ledger.Value                   as Value
+import           Prelude                        (IO, Show (..), String, Semigroup(..))
+import           Text.Printf                    (printf)
+import           Wallet.Emulator.Wallet
+import           Control.Monad.Freer.Extras     as Extras
+import           CustomRedeemer
 
 -- ON-CHAIN
 
 {-# INLINABLE tokenPolicy #-}
-
 -- Defining Minting Validator
-tokenPolicy :: PaymentPubKeyHash -> String -> () -> ScriptContext -> Bool
-tokenPolicy pkh collection () sContext = txSignedBy (scriptContextTxInfo sContext) $ unPaymentPubKeyHash pkh
+tokenPolicy :: RedeemerParam -> ScriptContext -> Bool
+tokenPolicy (RP outRef tokenName pkHash) sContext = traceIfFalse "Can not give the NFT"  givingPath 
+	where
+           givingPath :: Bool
+           givingPath = traceIfFalse "UTxO not consumed" hasUTxO &&
+                        traceIfFalse "Wrong ammount minted" checkMintedAmount &&
+                        traceIfFalse "Not original mint" checkIfAuthentic &&
+                        traceIfFalse "Reciever did not get the NFT" checkIfRecieved
+                        
+           info :: TxInfo
+           info = scriptContextTxInfo sContext
 
-policy :: PaymentPubKeyHash -> String -> Scripts.MintingPolicy Typed
-policy pkh collection = mkMintingPolicyScript $
-             		$$(PlutusTx.compile [|| \pkh' collection' -> Scripts.wrapMintingPolicy $ tokenPolicy pkh' collection' ||])
-             		`PlutusTx.applyCode`
-             		PlutusTx.liftCode pkh     
-             		`PlutusTx.applyCode`
-             		PlutusTx.liftCode collection  
+           hasUTxO :: Bool
+           hasUTxO = any (\utxo -> txInInfoOutRef utxo == outRef) $ txInfoInputs info
 
+           checkMintedAmount :: Bool
+           checkMintedAmount = case flattenValue (txInfoMint info) of
+              [(_, tokenName', amount)]   ->  tokenName' == tokenName && amount == 1
+              _                       ->  False  
+              
+           checkIfAuthentic :: Bool
+           checkIfAuthentic = txSignedBy (scriptContextTxInfo sContext) $ unPaymentPubKeyHash pkHash
+              
+           -- todo
+           checkIfRecieved :: Bool
+           checkIfRecieved = True        
+
+policy :: PaymentPubKeyHash -> Scripts.MintingPolicy
+policy pkHash = mkMintingPolicyScript $ $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy $ tokenPolicy ||]) 
 
 -- Extracting Policy ID    
-tokenSymbol :: PaymentPubKeyHash -> String -> CurrencySymbol
-tokenSymbol = \pkh' collection' -> scriptCurrencySymbol $ policy pkh' collection'
+tokenSymbol :: PaymentPubKeyHash -> CurrencySymbol
+tokenSymbol = \pkHash' -> scriptCurrencySymbol $ policy pkHash'
+
 
 -- OFF-CHAIN
+-- todo concat some unique value
+uniqueName :: TokenName -> TxOutRef -> TokenName
+uniqueName tn or = tn
 
 -- Defining minting lambda
-mint :: NFTMintParams -> Contract w NFTSchema Text ()
-mint mparams = do              
-               pkh <- Contract.ownPaymentPubKeyHash
-               let val = Value.singleton (tokenSymbol pkh (mCollection mparams)) (mToken mparams) 1
-                   lookups = Constraints.mintingPolicy (policy pkh (mCollection mparams))
-                   tx      = Constraints.mustMintValue val
-               ledgerTx <- submitTxConstraintsWith @Void lookups tx
-               void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-               Contract.logInfo @String $ printf "Forged %s" (show val)
+mint :: NFTParams -> Contract w NFTSchema Text ()
+mint mParams = do   
+               utxos <- utxosAt $ mAddress mParams  
+               pkHash <- Contract.ownPaymentPubKeyHash         
+               case Map.keys utxos of
+                   []       -> Contract.logError @String "No UTxO found on the provied Address!" 
+                   outRef : _ -> do 
+                                   let tokenName  = uniqueName (mToken mParams) outRef
+                                       redeemer   = (RP outRef tokenName pkHash)
+                                       nft        = Value.singleton (tokenSymbol pkHash) tokenName 1 
+                                       val        = (Ada.lovelaceValueOf 2_000_000) <> nft
+                                       lookups    = Constraints.mintingPolicy (policy pkHash)  <>
+                                                    Constraints.unspentOutputs utxos
+                                       tx         = Constraints.mustMintValueWithRedeemer (Redeemer $ PlutusTx.toBuiltinData redeemer) nft <>
+                                                    Constraints.mustSpendPubKeyOutput outRef <> 
+                                                    Constraints.mustPayToPubKey (PaymentPubKeyHash $ fromJust $ toPubKeyHash $ mReceiver mParams) val
+                                   ledgerTx <- submitTxConstraintsWith @Void lookups tx
+                                   void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+                                   Contract.logInfo @String $ printf "Forged %s" (show val)             
 
-
--- Defining minting input parameters
-data NFTMintParams = NFTMintParams
-    { mToken   :: !TokenName
-    , mAddress :: !Address
-    , mCollection :: !String
+-- Defining minting and dropping input parameters
+data NFTParams = NFTParams
+    { mToken    :: !TokenName
+    , mAddress  :: !Address
+    , mReceiver :: !Address
     } deriving (Generic, FromJSON, ToJSON, Show)
 
 
 -- Defining endpoint input binding
-type NFTSchema = Endpoint "mint" NFTMintParams  
+type NFTSchema = Endpoint "mint" NFTParams  
 
 
 -- Defining contract interaction endpoints
 endpoints :: Contract () NFTSchema Text ()
 endpoints = awaitPromise mint' >> endpoints
   where
-    mint' = endpoint @"mint" mint
-    
+    mint' = endpoint @"mint" mint    
 
 -- SIMULATOR
 
 test :: IO ()
 test = runEmulatorTraceIO $ do
-    let c = "CDP"
-        t1 = "Halloween_0001_L1"
+    let t1 = "Halloween_0001_L1"
         t2 = "Halloween_0002_L1"
         w1 = knownWallet 1
         w2 = knownWallet 2
     h1 <- activateContractWallet w1 endpoints
     h2 <- activateContractWallet w2 endpoints
-    callEndpoint @"mint" h1 $ NFTMintParams
+    callEndpoint @"mint" h1 $ NFTParams
         { mToken   = t1
         , mAddress = mockWalletAddress w1
-        , mCollection = c
+        , mReceiver = mockWalletAddress w2
         }
     void $ Emulator.waitNSlots 1
-    callEndpoint @"mint" h1 $ NFTMintParams
+    callEndpoint @"mint" h1 $ NFTParams
         { mToken   = t2
         , mAddress = mockWalletAddress w1
-        , mCollection = c
+        , mReceiver = mockWalletAddress w2
         }
     void $ Emulator.waitNSlots 1
