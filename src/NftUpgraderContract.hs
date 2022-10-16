901e964206acdbcacdf70395981ed6dc2d21fb2e
Zakhar Deyneka <95752742+zakhard90@users.noreply.github.com>
2022-10-16 23:00:22 +0200

    Added token name parsing wip

--- src/NftUpgraderContract.hs
+++ src/NftUpgraderContract.hs
@@ -39,12 +39,13 @@ import           Plutus.Trace.Emulator          as Emulator
 import qualified PlutusTx
 import           PlutusTx.Prelude               hiding (Semigroup(..), unless)
 import qualified PlutusTx.Builtins              as Builtins
+import qualified PlutusTx.Builtins.Class        as Class
 import           Ledger                         hiding (mint, singleton)
 import           Ledger.Constraints             as Constraints
 import qualified Ledger.Typed.Scripts           as Scripts
 import           Ledger.Ada                     as Ada
 import           Ledger.Value                   as Value
-import           Prelude                        (IO, Show (..), String, Semigroup(..))
+import           Prelude                        (IO, Show (..), String, Int, Semigroup(..), read, undefined)
 import           Text.Printf                    (printf)
 import           Wallet.Emulator.Wallet
 import           CustomRedeemer
@@ -52,8 +53,7 @@ import           CustomRedeemer
 -- ON-CHAIN
 
 
--- Defining Minting Validator
-  
+-- Defining Minting Validator  
 
 {-# INLINABLE tokenPolicy #-}
 tokenPolicy :: PaymentPubKeyHash -> RedeemerParam -> ScriptContext -> Bool
@@ -62,23 +62,54 @@ tokenPolicy pkHash (RP outRef tkName mintAmount) sContext = traceIfFalse "Can no
            givingPath :: Bool
            givingPath = traceIfFalse "UTxO not consumed" hasUTxO &&
                         traceIfFalse "Wrong ammount minted" checkMintedAmount &&
-                        traceIfFalse "Not original mint" checkIfAuthentic &&
+                        traceIfFalse "Not original or upgrade mint" checkIfOriginalOrUpgrade && 
                         traceIfFalse "Reciever did not get the NFT" checkIfRecieved
                         
            info :: TxInfo
            info = scriptContextTxInfo sContext
+           
+           utxoInputs :: [TxInInfo]
+           utxoInputs = txInfoInputs info
 
            hasUTxO :: Bool
-           hasUTxO = any (\utxo -> txInInfoOutRef utxo == outRef) $ txInfoInputs info
+           hasUTxO = any (\utxo -> txInInfoOutRef utxo == outRef) utxoInputs
   
            checkMintedAmount :: Bool
            checkMintedAmount = case flattenValue (txInfoMint info) of
               [(_, tkName', amount)]   ->  tkName' == tkName && amount == mintAmount
               _                        ->  False  
+           
+           checkIfOriginalOrUpgrade :: Bool
+           checkIfOriginalOrUpgrade = checkIfOriginal || checkIfUpgrade          
+             
+           checkIfUpgrade :: Bool
+           checkIfUpgrade = (>=) (length utxoInputs) 3
               
-           checkIfAuthentic :: Bool
-           checkIfAuthentic = txSignedBy (scriptContextTxInfo sContext) $ unPaymentPubKeyHash pkHash
+           checkIfOriginal :: Bool
+           checkIfOriginal = checkIfSigned && checkIfGenesis
               
+           checkIfSigned :: Bool
+           checkIfSigned = txSignedBy (scriptContextTxInfo sContext) $ unPaymentPubKeyHash pkHash
+           
+           tokenId :: BuiltinByteString
+           tokenId = Builtins.sliceByteString 10 7 $ unTokenName tkName
+           
+           tokenIndex :: BuiltinByteString
+           tokenIndex = Builtins.sliceByteString 0 4 tokenId
+           
+           tokenType :: BuiltinByteString
+           tokenType = Builtins.sliceByteString 5 1 tokenId
+           
+           --isSerum :: Bool
+           --isSerum = Class.fromBuiltin (Builtins.equalsByteString tokenType $ Class.fromString "S")
+           
+           tokenLevel :: BuiltinByteString
+           tokenLevel = Builtins.sliceByteString 6 1 tokenId
+           
+           checkIfGenesis :: Bool
+           checkIfGenesis = (==) (Builtins.lengthOfByteString $ unTokenName tkName) 26 && 
+                            (==) (Builtins.lengthOfByteString $ Builtins.sliceByteString 10 7 $ unTokenName tkName) 7
+           
            -- todo
            checkIfRecieved :: Bool
            checkIfRecieved = True        
@@ -114,15 +145,19 @@ uniqueName tn ou = TokenName $ Builtins.appendByteString (unTokenName tn) finger
            idx = Builtins.encodeUtf8 $ Builtins.toBuiltin $ pack $ show $ txOutRefIdx ou
            
 -- Defining minting function
-mintDrop :: NFTParams -> Contract w NFTSchema Text ()
+mintDrop :: NFTMintParams -> Contract w NFTSchema Text ()
 mintDrop mParams = do   
                utxos <- utxosAt $ mAddress mParams  
                pkHash <- Contract.ownPaymentPubKeyHash   
-               Contract.logInfo @String $ printf "PkHASH %s" (show pkHash)      
+  
                case Map.keys utxos of
                  []       -> Contract.logError @String "No UTxO found on the provied Address!" 
                  outRef : _ -> do 
                                 let tkName     = uniqueName (mToken mParams) outRef
+                                    tkId       = Builtins.sliceByteString 10 7 $ unTokenName tkName
+                                    tkIndex    = Builtins.sliceByteString 0 4 tkId
+                                    tkLevel    = Builtins.sliceByteString 5 2 tkId
+                                    
                                     mAmt       = mAmount mParams
                                     redeemer   = (RP outRef tkName mAmt)
                                     nft        = Value.singleton (tokenSymbol pkHash redeemer) tkName mAmt 
@@ -133,26 +168,41 @@ mintDrop mParams = do
                                                  Constraints.mustPayToPubKey (PaymentPubKeyHash $ fromJust $ toPubKeyHash $ mReceiver mParams) val
                                 ledgerTx <- submitTxConstraintsWith @Void lookups tx
                                 void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+                                
+                                Contract.logInfo @String $ printf "TokenName length %d" (Builtins.lengthOfByteString $ unTokenName tkName) 
+                                Contract.logInfo @String $ printf "ID %s" (show $ tkId)
+                                Contract.logInfo @String $ printf "Index %s" (show $ tkIndex)
+                                Contract.logInfo @String $ printf "Level %s" (show $ tkLevel)
                                 Contract.logInfo @String $ printf "Forged %s" (show val)             
 
+-- Defining minting function
+upgrade :: NFTUpgradeParams -> Contract w NFTSchema Text ()
+upgrade uParams = undefined  
+
 -- Defining minting and dropping input parameters
-data NFTParams = NFTParams
+data NFTMintParams = NFTMintParams
     { mToken    :: !TokenName
     , mAmount   :: !Integer
     , mAddress  :: !Address
     , mReceiver :: !Address
     } deriving (Generic, FromJSON, ToJSON, Show)
 
+-- Defining nft upgrading input parameters
+data NFTUpgradeParams = NFTUpgradeParams
+    { uPolicy   :: !TokenName
+    , uAddress  :: !Address
+    } deriving (Generic, FromJSON, ToJSON, Show)
 
 -- Defining endpoint input binding
-type NFTSchema = Endpoint "mint" NFTParams  
+type NFTSchema = Endpoint "mint" NFTMintParams .\/ Endpoint "upgrade" NFTUpgradeParams 
 
 
 -- Defining contract interaction endpoints
 endpoints :: Contract () NFTSchema Text ()
-endpoints = awaitPromise mint' >> endpoints
+endpoints = awaitPromise (mint' `select` upgrade') >> endpoints
   where
-    mint' = endpoint @"mint" mintDrop    
+    mint' = endpoint @"mint" mintDrop
+    upgrade' = endpoint @"upgrade" upgrade     
 
 -- SIMULATOR
 
@@ -163,30 +213,39 @@ test = runEmulatorTraceIO $ do
         q1 = 1
         t2 = "Halloween_0002_L1_"
         q2 = 1
-        t3 = "Halloween_S1_"
-        q3 = 1000
+        t3 = "Halloween_0001_L2_"
+        q3 = 1
+        s1 = "Halloween_0000_S1_"
+        qs = 1000
         w1 = knownWallet 1
         w2 = knownWallet 2
     h1 <- activateContractWallet w1 endpoints
     h2 <- activateContractWallet w2 endpoints
-    callEndpoint @"mint" h1 $ NFTParams
+    callEndpoint @"mint" h1 $ NFTMintParams
         { mToken    = t1
         , mAmount   = q1
         , mAddress  = mockWalletAddress w1
         , mReceiver = mockWalletAddress w2
         }
     void $ Emulator.waitNSlots 1
-    callEndpoint @"mint" h2 $ NFTParams
+    callEndpoint @"mint" h1 $ NFTMintParams
         { mToken   = t2
         , mAmount   = q2
-        , mAddress = mockWalletAddress w2
-        , mReceiver = mockWalletAddress w2
-        }
-    void $ Emulator.waitNSlots 1
-    callEndpoint @"mint" h1 $ NFTParams
-        { mToken   = t3
-        , mAmount   = q3
         , mAddress = mockWalletAddress w1
         , mReceiver = mockWalletAddress w2
         }
     void $ Emulator.waitNSlots 1
+    callEndpoint @"mint" h1 $ NFTMintParams
+        { mToken   = s1
+        , mAmount   = qs
+        , mAddress = mockWalletAddress w1
+        , mReceiver = mockWalletAddress w2
+        }
+    void $ Emulator.waitNSlots 1
+    callEndpoint @"mint" h2 $ NFTMintParams
+        { mToken   = t3
+        , mAmount   = q3
+        , mAddress = mockWalletAddress w2
+        , mReceiver = mockWalletAddress w2
+        }
+    void $ Emulator.waitNSlots 1          
